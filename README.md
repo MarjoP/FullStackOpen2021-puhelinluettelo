35faffc5b4def7a2d19d6c15a05e9a8d61aacae3
Marjo Pantsar <marjo.pantsar@gmail.com>
2021-10-03 11:31:59 +0300

    finish exercises 3

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # FullStackOpen2021-puhelinluettelo
 
 -osa3
-    - [3.1 . 3.19 Puhelinluettelo backend](/)
+    - [3.1-3.22 Puhelinluettelo backend](/)
--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 console.log('Well, hello there my little friend!')
 require('dotenv').config()
 const Person = require('./models/person')
-
-const { response, application } = require('express')
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
-const { prependOnceListener } = require('./models/person')
 morgan.token('object', function getObject(req) {
     if(req.method === 'POST')
-    return JSON.stringify(req.body);
+        return JSON.stringify(req.body)
 })
 const errorHandler = (error, request, response, next) => {
     console.error(error.message)
     if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-      return response.status(400).json( { error: error.message })
+        return response.status(400).send({ error: 'malformatted id' })
+    }   else if (error.name === 'ValidationError') {
+        return response.status(400).json( { error: error.message })
     }
     next(error)
-  }
+}
 
 const app = express()
 const PORT = process.env.PORT
@@ -29,75 +26,67 @@ app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :object'))
 app.use(errorHandler)
 
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`)
-    })
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
 
-    app.get('/info', (req, res, next) => {
-            Person.find({}).then(persons => {
-               const amount = persons.length
-               res.send(`<div><p>Phonebook has info for ${amount} people</p><p>${new Date()}</p></div>`)
-            })
-            .catch(error => next(error))
-           
+app.get('/info', (req, res, next) => {
+    Person.find({}).then(persons => {
+        const amount = persons.length
+        res.send(`<div><p>Phonebook has info for ${amount} people</p><p>${new Date()}</p></div>`)
+    }).catch(error => next(error))
+})
 
-    })
+app.get('/api/persons', (req, res, next) => {
+    Person.find({}).then(persons => {
+        res.json(persons)
+    }).catch(error => next(error))
+})
 
-    app.get('/api/persons', (req, res, next) => {
-        Person.find({}).then(persons => {
-            res.json(persons)
-        })
-        .catch(error => next(error))
-    })
-
-    app.get('/api/persons/:id', (req, res, next) => {
-        Person.findById(req.params.id).then(person => {
-            if(person) {
-                res.json(person)
-            } else {
-                res.status(404).end()
-            }
-        })
-        .catch(error => next(error))
-    })
-
-    app.delete('/api/persons/:id', (req, res, next) => {
-        Person.findByIdAndRemove(req.params.id)
-            .then(result => {
-                res.status(204).end()
-            })
-            .catch(error => next(error))
-    })
-      
-    app.post('/api/persons', (req, res, next) => {
-        const body = req.body;
-
-        if(body.name === undefined) {
-            return res.status(400).json({error: 'content missing'})
+app.get('/api/persons/:id', (req, res, next) => {
+    Person.findById(req.params.id).then(person => {
+        if(person) {
+            res.json(person)
+        } else {
+            res.status(404).end()
         }
+    }).catch(error => next(error))
+})
 
-        const person = new Person({
-            name: body.name,
-            number: body.number || "",
-        })
+app.delete('/api/persons/:id', (req, res, next) => {
+    Person.findByIdAndRemove(req.params.id)
+        .then(() => {
+            res.status(204).end()
+        }).catch(error => next(error))
+})
 
-        person.save().then(savedPerson => {
-           res.json(savedPerson)
-        })
-        .catch(error => next(error))
+app.post('/api/persons', (req, res, next) => {
+    const body = req.body
+
+    if(body.name === undefined) {
+        return res.status(400).json({ error: 'content missing' })
+    }
+
+    const person = new Person({
+        name: body.name,
+        number: body.number || '',
     })
 
-    app.put('/api/persons/:id', (req, res, next) => {
-        const body = req.body
+    person.save().then(savedPerson => {
+        res.json(savedPerson)
+    }).catch(error => next(error))
+})
 
-        const person = {
-            name: body.name,
-            number: body.number,
-        }
+app.put('/api/persons/:id', (req, res, next) => {
+    const body = req.body
 
-        Person.findByIdAndUpdate(req.params.id, person, {new: true})
-            .then(updatedPerson => {
-                res.json(updatedPerson)
-            })
-            .catch(error => next(error))
-    })
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+
+    Person.findByIdAndUpdate(req.params.id, person, { new: true })
+        .then(updatedPerson => {
+            res.json(updatedPerson)
+        }).catch(error => next(error))
+})
